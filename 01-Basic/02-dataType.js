4847ca65113275e6abb1225accee8324fe8e74a0
Sukanto Mahato <117590094+sukanto088@users.noreply.github.com>
2024-02-08 19:47:09 +0000

    datatypes

---
+++ 01-Basic/02-dataType.js
@@ -0,0 +1,19 @@
+"use strict";//treat all JS code as newer version
+// alert(3+3) we are using node js not browser
+ let name ="sukanto"
+ let age =21
+ let  isLoggedIn= false;  
+
+
+ /*      Datatypes
+number=>2 to power 53
+bigint
+string => ""
+boolean
+null = >standalone value
+undefined = > when value is not defined
+symbol  =>to find uniquness
+object
+ */
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
