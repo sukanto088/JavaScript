81d0b94ba4881f639f71fea5cbea240abdeb7036
Sukanto Mahato <117590094+sukanto088@users.noreply.github.com>
2024-02-09 05:01:40 +0000

    ConversionOperator

--- 03-ConversionOperator.js
+++ 03-ConversionOperator.js
@@ -15,6 +15,36 @@
 //1=> true; 0=> false
 //" " =>  false
 //"sukanto "=>true
-let someNumber ="33";
-let stringNumeber= String(someNumber)
-console.log(typeof stringNumeber);
+// let someNumber ="33";
+// let stringNumeber= String(someNumber)
+// console.log(typeof stringNumeber);
+
+//***************************Operations********************************** */
+// let value=3;
+// let negValue= -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2/2)
+// console.log(2*2)
+// console.log(2**2)
+
+
+// let str1= "sukanto";
+// let str2="mahato";
+// let str3 =str1 +str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+console.log(+true); //1
+console.log(+"") //0
+
+let num1,num2,num3
+num1=num2=num3= 2 + 2;
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
