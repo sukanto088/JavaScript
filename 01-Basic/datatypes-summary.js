ec2b43a9cfb938b9aecb76bfa3c603d6f956aed9
Sukanto Mahato <117590094+sukanto088@users.noreply.github.com>
2024-02-11 04:08:44 +0000

    datatypes Summary

---
+++ 01-Basic/datatypes-summary.js
@@ -0,0 +1,23 @@
+//Primitive Datatypes
+
+//7 types: String , Number, Boolean , null ,undefined ,Symbol ,BigInt 
+
+const score =100;
+const scoreValue =100.3
+const isLoggedIn =false
+let userEmail =undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id==anotherId); //false
+
+//Reference (Non primitive)
+
+//Array , objects , Functions 
+//Array
+const heros =['ben10','shakitman']
+//object
+let myobj={
+    name:sukanto,
+    age:21,
+}
