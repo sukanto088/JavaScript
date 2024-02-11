ee958477aea8e3a2c68142ec42c34f77a652fa9a
Sukanto Mahato <117590094+sukanto088@users.noreply.github.com>
2024-02-11 07:33:41 +0000

    datatypes

--- 01-Basic/02-dataType.js
+++ 01-Basic/02-dataType.js
@@ -1,7 +1,7 @@
 "use strict";//treat all JS code as newer version
 // alert(3+3) we are using node js not browser
  let name ="sukanto"
- let age =21
+ let age =21 //number
  let  isLoggedIn= false;  
 
 
