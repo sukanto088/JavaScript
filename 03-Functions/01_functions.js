1d8309a93f9d5a89e031b7b4dde4d46c35ac0290
Sukanto Mahato <117590094+sukanto088@users.noreply.github.com>
2024-02-12 08:21:48 +0000

    functions with objects and Arrays

--- 03-Functions/01_functions.js
+++ 03-Functions/01_functions.js
@@ -30,4 +30,32 @@ function loginUserMessage(username){
     return `${username} just logged in `
 }
 // console.log(loginUserMessage("sukanto"));
-console.log(loginUserMessage());//undefined
+// console.log(loginUserMessage());//undefined
+
+function calculateCartPrice(...num1)
+{
+    return num1
+}
+// console.log(calculateCartPrice(200,300,400));
+
+const user={
+    username:"sukanto",
+    price:199
+}
+function handelObject(anyobject)
+{
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handelObject(user)
+// handelObject({
+//     username:"sukanto",
+//     price:200
+
+// })
+
+// const mynewArr=[200,300,400,100,600]
+// function returnSecondValue(getArray)
+// {
+//     return getArray[1]
+// }
+// console.log(returnSecondValue(mynewArr));
