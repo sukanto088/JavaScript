f54485ff17203232867911a56198173bc40b2e3b
Sukanto Mahato <117590094+sukanto088@users.noreply.github.com>
2024-02-16 08:12:56 +0000

    while and do while loop

---
+++ 05-Iterations/two.js
@@ -0,0 +1,12 @@
+let index =0
+while (index <=10) {
+    console.log(`value of index is ${index}`);
+    index= index+2
+}
+let myArray =['flash','batman','bem10']
+let arr =0;
+while(arr<myArray.length)
+{
+    console.log(`value is ${myArray[arr]}`)
+    arr=arr+1
+}
