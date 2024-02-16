efadda766ede635680d4fd2e8255c8931dd8ad5f
Sukanto Mahato <117590094+sukanto088@users.noreply.github.com>
2024-02-16 05:23:50 +0000

    If-else Condition

---
+++ 04-Control-Flow/one.js
@@ -0,0 +1,56 @@
+// const isUserLoggedIn =true
+const temperature =41
+// if(2=="2")
+// {
+//     console.log("execute");
+// }
+// if(2!=3)
+// {
+//     console.log("execute");
+// }
+// if(temperature===41)
+// {
+//     console.log("less than 50");
+// }
+// else{
+//     console.log("temperature greater than 50");
+// }
+// console.log("execute");
+
+// const score =200
+// if(score>100)
+// {
+//     let power ="fly"
+//     console.log(`user power :${power}`);
+// }
+const balance =1000
+// if(balance>500) console.log("test");
+// if(balance<500)
+// {
+//     console.log("less than 500");
+// }
+// else if(balance<750)
+// {
+//     console.log("less than 750");
+// }
+// else if(balance<900)
+// {
+//     console.log("less than 750");
+// }
+// else{
+//     console.log("less than 1200");
+// }
+
+const UserLoggedIn= true
+const debitcard = true
+const loggedinfromgoogle=false
+const loggedinfromemail =true
+if(UserLoggedIn && debitcard)
+{
+    console.log("allow to buy course");
+}
+if(loggedinfromemail || loggedinfromgoogle)
+{
+    console.log("user logged in ");
+}
+
