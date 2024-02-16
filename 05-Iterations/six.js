ede8a30cb606dcf2c228933aaadd25008880a581
Sukanto Mahato <117590094+sukanto088@users.noreply.github.com>
2024-02-16 09:59:08 +0000

    filter method in js

---
+++ 05-Iterations/six.js
@@ -0,0 +1,25 @@
+// const coding =['js','rb','py','java','cpp']
+
+// const values = coding.forEach((item)=>{
+//     // console.log(item);
+//     return item 
+// })
+
+// console.log(values)
+
+const myNumbs = [1,2,3,4,5,6, 7,8,9,10]
+
+// const newnums= myNumbs.filter((num)=>{
+//      return num>4
+// })
+
+
+
+// const newnums =[]
+// myNumbs.forEach((num)=>{
+//     if(num>4){
+//         newnums.push(num)
+//     }
+// })
+// console.log(newnums);
+
