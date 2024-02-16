9dc5924ecc5d947dfb3e64ac0307891b9b071a03
Sukanto Mahato <117590094+sukanto088@users.noreply.github.com>
2024-02-16 08:47:32 +0000

    forin loop in js

---
+++ 05-Iterations/four.js
@@ -0,0 +1,22 @@
+// const myobject ={
+//     js:'jsvascript',
+//     cpp:"c++",
+//     rb:"ruby",
+//     swift:"swift by apple"
+// }
+// for (const key in myobject) {
+//     console.log(`${key} shortcut is for ${myobject[key]}`);
+// }
+
+// const programming =['js','rb','py','java','cpp']
+// for (const key in programming) {
+//     console.log(programming[key])
+// }
+
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('USA',"United States of America")
+
+// for (const key in map) {
+//    console.log(key);
+// }
